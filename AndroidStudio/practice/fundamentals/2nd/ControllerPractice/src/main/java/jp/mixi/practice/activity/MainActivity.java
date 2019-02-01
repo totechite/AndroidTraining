faa204188608433476769b4a130ccfb5febff39d
totechite <yutan.h9@gmail.com>
2019-02-01 20:29:43 +0900

    やってる

--- AndroidStudio/practice/fundamentals/2nd/ControllerPractice/src/main/java/jp/mixi/practice/activity/MainActivity.java
+++ AndroidStudio/practice/fundamentals/2nd/ControllerPractice/src/main/java/jp/mixi/practice/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package jp.mixi.practice.activity;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // FragmentManager も、android.support.v4.app.FragmentManager を利用する
+        FragmentManager manager = getSupportFragmentManager();
+        // レイアウトから Fragment を見つけ出してインスタンスを得る
+        android.support.v4.app.Fragment fragment = manager.findFragmentById(R.id.MyFragment);
     }
 }
+
